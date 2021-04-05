870091150aee3ccee47435a7feeb4651fa3c61da
yechielby <yechielby@gmail.com>
2021-04-05 21:49:52 +0300

    EX1

---
+++ src/Products.js
@@ -0,0 +1,22 @@
+import Product from './Product';
+import './Products.css';
+
+function Products() {
+    return (
+        <section className="products">
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+
+        </section>
+    );
+}
+
+export default Products;
